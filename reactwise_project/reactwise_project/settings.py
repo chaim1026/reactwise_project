38eb1fcc299c5d5e8e77597931b9f4451f478a09
Chaim <chaim1026@gmail.com>
2020-10-07 20:36:14 +0300

    fix for heroku

--- reactwise_project/reactwise_project/settings.py
+++ reactwise_project/reactwise_project/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,3 +137,5 @@ except ImportError:
 
 LOGIN_REDIRECT_URL = 'homepage'
 LOGOUT_REDIRECT_URL = 'welcome'
+
+django_heroku.settings(locals())
